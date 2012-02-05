459348d131c7cb8021c793a3eb89ba659e056578
Erik Westrup <erik.westrup@gmail.com>
2012-02-05 21:05:27 +0100

    Buildfile for the client.

---
+++ client/src/Client.java
@@ -0,0 +1,15 @@
+public class Client { 
+
+	public static void main(String args[]) {
+		new Client().run();
+	}
+
+	public Client() {
+
+	}
+
+	public void run() {
+		System.out.println("hi!");
+	}
+
+}
