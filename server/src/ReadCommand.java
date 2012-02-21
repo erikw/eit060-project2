7839414d0a782ad3182a8cc4d0cf68698215e955
Erik Westrup <erik.westrup@gmail.com>
2012-02-21 16:57:15 +0100

    Logging for read command.

--- server/src/ReadCommand.java
+++ server/src/ReadCommand.java
@@ -17,20 +17,30 @@ public class ReadCommand extends Command {
 		switch (subject.charAt(0)) {
 			case 'a':
 					 canDo = true;
+					 log.info("Agency reads record " + recordID);
 					 break;
 			case 'd':
 					 if (subject.equals(record.doctorID) || record.departmentID.equals(departmentID)) {
 						canDo = true;
+						 log.info("Doctor reads record " + recordID);
+					 } else {
+						 log.severe("Doctor was denied reading record " + recordID);
 					 }
 					 break;
 			case 'n':
 					 if (subject.equals(record.nurseID) || record.departmentID.equals(departmentID)) {
 						canDo = true;
+						 log.info("Nurse reads record " + recordID);
+					 } else {
+						 log.severe("Nurse was denied reading record " + recordID);
 					 }
 					 break;
 			default: // patient
 					 if (record.patientID.equals(subject)) {
 						 canDo = true;
+						 log.info("Patient reads record " + recordID);
+					 } else {
+						 log.severe("Patient was denied reading record " + recordID);
 					 }
 		}
 		if (canDo) {
