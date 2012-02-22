1ee67be719b15b7aa1f4bf37b799a2641351b738
Erik Jansson <erikjansson90@gmail.com>
2012-02-22 09:14:19 +0100

    Fixed regex in ListCommand

--- server/src/server/ListCommand.java
+++ server/src/server/ListCommand.java
@@ -15,38 +15,38 @@ public class ListCommand extends Command {
 	}
 
 	public String execute(Map<Integer, Record> records) {
-	    
-	    if(subject.matches("[0-9]+")) {
-		if(patientID.equals(NO_ID)) {
-		    //TODO Add logging messages here?
-		    return getAllRecords(records, subject);
+
+		if(subject.matches("[0-9]{6}-[0-9]{4}")) {
+			if(patientID.equals(NO_ID)) {
+				//TODO Add logging messages here?
+				return getAllRecords(records, subject);
+			} else {
+				//TODO Add logging messages here?
+				return "No journals available for you.";
+			}
 		} else {
-		    //TODO Add logging messages here?
-		    return "No journals available for you.";
+			if(patientID.equals(NO_ID)) {
+				//TODO Add logging messages here?
+				return "Add a patientID dumbass.";
+			}
+			//TODO add logging messages here?
+			return getAllRecords(records, patientID);
 		}
-	    } else {
-		if(patientID.equals(NO_ID)) {
-		    //TODO Add logging messages here?
-		    return "Add a patientID dumbass.";
-		}
-		//TODO add logging messages here?
-		return getAllRecords(records, patientID);
-	    }
-	    
+
 	}
 
-    
-    private String getAllRecords(Map<Integer, Record> records, String getID) {
-	StringBuilder sb = new StringBuilder();
-	String search = getID;
-	Collection<Record> list = records.values();
-	
-	for(Record rec : list) {
-	    if(rec.patientID.equals(search)) {
-		sb.append(rec.recordID).append(" ");
-		//TODO Add logging messages here?
-	    }
+
+	private String getAllRecords(Map<Integer, Record> records, String getID) {
+		StringBuilder sb = new StringBuilder();
+		String search = getID;
+		Collection<Record> list = records.values();
+
+		for(Record rec : list) {
+			if(rec.patientID.equals(search)) {
+				sb.append(rec.recordID).append(": ").append(rec.recordText).append(" ");
+				//TODO Add logging messages here?
+			}
+		}
+		return sb.toString();
 	}
-	return sb.toString();
-    }
 }
